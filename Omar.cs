7993c5e2da686607a388da927ed3863217d8f26c
Omar_Ameer <omar.ameer244@gmail.com>
2022-12-14 22:21:48 +0200

    Test 2

--- Omar.cs
+++ Omar.cs
@@ -1,1 +1,32 @@
-Console.WriteLine("OOP Exercises");
+using System;
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        public double length;
+        public double width;
+        public double GitArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Area: " + GitArea());
+        }
+    }
+
+    class ExecuteRectanle
+    {
+        static void Main(String[] args)
+        {
+            Rectangle r = new Rectangle();
+
+            r.length = 4.5;
+            r.width = 3.5;
+
+            r.Display();
+        }
+    }
+}
