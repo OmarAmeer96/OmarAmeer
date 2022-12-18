2f438c23fa2fb565869b8582a581cd842646e3c6
Omar_Ameer <omar.ameer244@gmail.com>
2022-12-18 17:44:53 +0200

    Add Full Task 1 to Tests File

--- Tests.cs
+++ Tests.cs
@@ -979,6 +979,73 @@
 // }
 
 // ----------------------------------------------------------------------------------------
+
+// Task 1, Part 4:
+// Tasks -- Omar Ameer Mohamed Ameen Mohamed -- Sec 6
+// using System;
+// namespace Tasks
+// {
+//     public abstract class Person
+//     {
+//         public string Name;
+//         public int Age;
+//         public Person(string name, int age)
+//         {
+//             Name = name;
+//             Age = age;
+//         }
+//         public abstract void Print();
+//     }
+
+//     public class Student : Person
+//     {
+//         public int Year;
+//         public float Gpa;
+//         public Student(string name, int age, int year, float gpa) : base(name, age)
+//         {
+//             Year = year;
+//             Gpa = gpa;
+//         }
+//         public override void Print()
+//         {
+//             Console.WriteLine($"My name is {Name}, my age is {Age}, my gpa is {Gpa}");
+//         }
+//     }
+//     public class Database
+//     {
+//         private int _currentIndex;
+//         public Person[] Peaple = new Person[50];
+//         public void AddStudent(Student student){
+//             Peaple[_currentIndex++] = student;
+//         }
+//     }
+
+//     public class Program
+//     {
+//         private static void Main(string[] args)
+//         {
+//             var database = new Database();
+
+//             Console.Write("Please, enter student's name: ");
+//             var name = Console.ReadLine();
+//             Console.Write("Please, enter student's age: ");
+//             var age = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("Please, enter student's year: ");
+//             var year = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("Please, enter student's gpa: ");
+//             var gpa = Convert.ToSingle(Console.ReadLine());
+
+//             var student = new Student(name!, age, year, gpa);
+
+//             database.AddStudent(student);
+//         }
+//     }
+// }
+
 // ----------------------------------------------------------------------------------------
+
+// Task 2, Part 1:
+
+
 // ----------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------
