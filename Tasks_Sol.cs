a53484bda82d612cd5cdff493b71ed797604c958
Omar_Ameer <omar.ameer244@gmail.com>
2022-12-18 15:14:48 +0200

    Task 1

--- Tasks_Sol.cs
+++ Tasks_Sol.cs
@@ -1,1 +1,16 @@
-// Console.WriteLine("Tasks Here.");
+// Tasks -- Omar Ameer Mohamed Ameen Mohamed -- Sec 6
+using System;
+namespace Tasks
+{
+    public abstract class Person
+    {
+        string Name;
+        int Age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public abstract void Print();
+    }
+}
