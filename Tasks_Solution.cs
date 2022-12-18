5b350935e208b63b3f56d3180b6ea930342db1f6
Omar_Ameer <omar.ameer244@gmail.com>
2022-12-18 17:42:48 +0200

    Final Task 1 Sol

--- Tasks_Solution.cs
+++ Tasks_Solution.cs
@@ -14,7 +14,7 @@ namespace Tasks
         public abstract void Print();
     }
 
-    public abstract class Student : Person
+    public class Student : Person
     {
         public int Year;
         public float Gpa;
@@ -25,18 +25,9 @@ namespace Tasks
         }
         public override void Print()
         {
-        }
-    }
-
-    public sealed class P1 : Student
-    {
-        public P1(string name, int age, int year, float gpa) : base(name, age, year, gpa) { }
-        public override void Print()
-        {
             Console.WriteLine($"My name is {Name}, my age is {Age}, my gpa is {Gpa}");
         }
     }
-
     public class Database
     {
         private int _currentIndex;
@@ -50,17 +41,20 @@ namespace Tasks
     {
         private static void Main(string[] args)
         {
-            Console.Write("Please, enter your name: ");
+            var database = new Database();
+
+            Console.Write("Please, enter student's name: ");
             var name = Console.ReadLine();
-            Console.Write("Please, enter your age: ");
+            Console.Write("Please, enter student's age: ");
             var age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Please, enter your year: ");
+            Console.Write("Please, enter student's year: ");
             var year = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Please, enter your gpa: ");
+            Console.Write("Please, enter student's gpa: ");
             var gpa = Convert.ToSingle(Console.ReadLine());
 
-            P1 obj = new P1(name!, age, year, gpa);
-            obj.Print();
+            var student = new Student(name!, age, year, gpa);
+
+            database.AddStudent(student);
         }
     }
 }
