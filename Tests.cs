348d0a1be7396a391ad7d91f12ec9f450044390a
Omar_Ameer <omar.ameer244@gmail.com>
2022-12-18 11:39:33 +0200

    Sec 2 oop, Test 5, Edit 1

--- Tests.cs
+++ Tests.cs
@@ -467,6 +467,76 @@ namespace ObjectOriented
         }
     }
 
+    public sealed class Tesla : ElectricCar
+    // Sealed Class ==> is the last class of the chain of classes ==> is a class that no one can inherit it
+    {
+        public Tesla(string? color, int battery) : base("Tesla", color, battery) // override for constructors
+        {
+            Color = color;
+            Battery = battery;
+        }
+        public override void start()
+        {
+            Console.WriteLine("Starting...");
+        }
+    }
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Color: ");
+            var color = Console.ReadLine();
+            Console.Write("Battery: ");
+            var battery = Convert.ToInt32(Console.ReadLine());
+            var obj = new Tesla(color, battery);
+            obj.print();
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------------------
+
+// Abstract ==> part 2
+using System;
+namespace ObjectOriented
+{
+    public abstract class Car
+    {
+        public int Battery;
+        public string? Name;
+        public string? Color;
+
+        public Car() { }
+        public Car(string? name, string? color)
+        {
+            Name = name;
+            Color = color;
+        }
+        public virtual void print() // Virtual ==> I can not (override) this method without (virtual)
+        {
+            Console.WriteLine($"Car: {Name}, Color: {Color}");
+        }
+        public abstract void start(); // Abstract because this method does not have implementation, and the class contains it shoud be abstract too
+    }
+
+    public abstract class ElectricCar : Car
+    // class ElectricCar (Inherits) class Car.
+    // if in this class i do not wnt to implement the start method too so i should make this class abstract too
+    {
+        public ElectricCar(string? name, string? color, int battery)
+        {
+
+            Name = name;
+            Color = color;
+            Battery = battery;
+        }
+
+        public override void print()
+        {
+            Console.WriteLine($"Car: {Name}, Color: {Color}, Battery: {Battery}");
+        }
+    }
+
     public class Tesla : ElectricCar
     {
         public Tesla(string? color, int battery) : base("Tesla", color, battery) // override for constructors
